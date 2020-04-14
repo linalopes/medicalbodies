2aa111349f55a12c724cfd0b255337b693be2f1e
Lina Lopes <lina@lilo.zone>
2020-04-14 03:16:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -123,25 +123,3 @@
 
 
 
-
-
-
-
-
-jQuery(document).ready(function(){
-
-	/* Open lightbox on button click */
-	$('.lightbox-toggle').click(function(){
-		$('.backdrop').animate({'opacity':'.50'}, 300, 'linear');
-		$('.box').fadeIn();
-	});
-
-	/* Click to close lightbox */
-	$('.close, .backdrop').click(function(){
-		$('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
-			$('.backdrop').css('display', 'none');
-		});
-		$('.box').fadeOut();
-	});
-
-});
